add971f77c24f8b8c792239747e0ad99931f3e45
Sptangs <aseptian553@gmail.com>
2026-05-15 18:32:07 +0700

    feature/models-User

--- src/models/User.java
+++ src/models/User.java
@@ -1,25 +1,105 @@
 package models;
 
 public class User {
-    public int id_user;
-    public String nama;
-    public String email;
-    public String password;
-    public String alamat;
 
-    public void mengisiData(int id_user, String nama, String email, String password, String alamat) {
-        this.id_user = id_user;
+    public enum Role {
+        ADMIN,
+        STAFF,
+        MANAGER,
+        WORKER
+    }
+
+    private int idUser;
+    private String nama;
+    private int age;
+    private String email;
+    private String password;
+    private String alamat;
+    private String noHp;
+    private Role role;
+
+    public User() {}
+
+    public User(int idUser,
+                String nama,
+                int age,
+                String email,
+                String password,
+                String alamat,
+                String noHp,
+                Role role) {
+
+        this.idUser = idUser;
         this.nama = nama;
+        this.age = age;
         this.email = email;
         this.password = password;
         this.alamat = alamat;
+        this.noHp = noHp;
+        this.role = role;
     }
 
-    public void tampilInfo() {
-        System.out.println("===== DATA USER =====");
-        System.out.println("ID User   : " + id_user);
-        System.out.println("Nama      : " + nama);
-        System.out.println("Email     : " + email);
-        System.out.println("Alamat    : " + alamat);
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public String getNoHp() {
+        return noHp;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public void setNoHp(String noHp) {
+        this.noHp = noHp;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
     }
 }

4a9330357d0972f5bfc4a94d8f20c4f99fdff148
12Krisna <wahyukrisna926@gmail.com>
2026-05-17 17:09:45 +0700

    Krisna menambah tabel dan getter untuk controller

--- src/views/PerusahaanView.java
+++ src/views/PerusahaanView.java
@@ -1,11 +1,14 @@
 package views;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 
 public class PerusahaanView extends JFrame {
     private JTextField txtId, txtNama, txtAlamat, txtEmail;
     private JButton btnAdd, btnUpdate, btnDelete, btnClear;
+    private JTable table;
+    private DefaultTableModel tableModel;
 
     public PerusahaanView() {
         setTitle("Manajemen Perusahaan - CariKerja");
@@ -47,11 +50,28 @@ public class PerusahaanView extends JFrame {
         buttonPanel.add(btnClear);
 
         //tambah panel ke frame
+        tableModel = new DefaultTableModel(new String[]{"ID", "Nama", "Alamat", "Email"}, 0);
+        table = new JTable(tableModel);
+        JScrollPane scrollPane = new JScrollPane(table);
+
         add(formPanel, BorderLayout.NORTH);
         add(buttonPanel, BorderLayout.CENTER);
+        add(scrollPane, BorderLayout.SOUTH);
 
         setVisible(true);
     }
+
+    //getter controller
+    public JTextField getTxtId() { return txtId; }
+    public JTextField getTxtNama() { return txtNama; }
+    public JTextField getTxtAlamat() { return txtAlamat; }
+    public JTextField getTxtEmail() { return txtEmail; }
+    public JButton getBtnAdd() { return btnAdd; }
+    public JButton getBtnUpdate() { return btnUpdate; }
+    public JButton getBtnDelete() { return btnDelete; }
+    public JButton getBtnCLear() { return btnClear; }
+    public JTable getTable() { return table; }
+    public DefaultTableModel getTableModel() { return tableModel; }
     
     public static void main(String[] args) {
         new PerusahaanView();
